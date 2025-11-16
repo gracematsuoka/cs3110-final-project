8f8e2db40ea05e3524e79e4327d1c1f5d3943c81
Elisa Pan <ep554@cornell.edu>
2025-11-16 16:32:21 -0500

    Add ship lists to initialize.mli

--- lib/initialize.mli
+++ lib/initialize.mli
@@ -2,7 +2,7 @@
 
 type ship = {
   name : string;  (** Ship name: "0a"…"0e" or "1a"…"1e". *)
-  coords : (int * int) list;  (** List of (row, col) cells occupied. *)
+  mutable coords : (int * int) list;  (** List of (row, col) cells occupied. *)
 }
 (** A ship with a name and its occupied coordinates. *)
 
@@ -12,6 +12,11 @@ type grid_state =
   | HIT
   | MISS
   | SINK
+
+val ship_list0 : ship list
+(** list of 5 ships for player0 *)
+val ship_list1 : ship list
+(** list of 5 ships for player1 *)
 
 val board_list : grid_state array array list
 (** list of boards board_list[0] is player0 personal board; board_list[1] is
