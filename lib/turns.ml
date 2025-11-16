0b37d70bbda2c5adee7c82fea9b79868777618bd
Elisa Pan <ep554@cornell.edu>
2025-11-16 15:50:00 -0500

    Add function stubs for turns.ml

---
+++ lib/turns.ml
@@ -0,0 +1,11 @@
+let validate_coord (r, c) = failwith "Unimplemented"
+
+let update_personal_board (r, c) (hit_type : Initialize.grid_state) =
+  failwith "Unimplemented"
+
+let update_attack_board (r, c) (hit_type : Initialize.grid_state) =
+  failwith "Unimplemented"
+
+let remove_coord (r, c) ship_coords = failwith "Unimplemented"
+let check_win ship_coords = failwith "Unimplemented"
+let handle_turn (r, c) player = failwith "Unimplemented"
