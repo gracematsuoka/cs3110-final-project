44c4bb76acfad19ec583a2ea5f1b0eb10bca391e
Elisa Pan <ep554@cornell.edu>
2025-11-16 18:33:45 -0500

    Moved handle_turn documentation to turns.mli

--- lib/turns.ml
+++ lib/turns.ml
@@ -84,19 +84,6 @@ let check_win (ship_coords : Initialize.ship list) : bool =
     (fun (s : Initialize.ship) -> Initialize.CoordSet.is_empty s.coords)
     ship_coords
 
-(** [handle_turn (r,c) player] processes a move where [player] attacks
-    coordinate [(r,c)].
-
-    - If [(r,c)] is out of bounds or has already been attacked, the move is
-      invalid and [player] goes again.
-    - If [(r,c)] is EMPTY on the other player's personal board, the attack is a
-      MISS and the other player goes next.
-    - If [(r,c)] is SHIP on other player's personal board, the attack is a HIT
-      or a SINK: if the other player still has remaining ships, [player] foes
-      again; if the other player has no ships left, [player] wins.
-
-    Requires [player] is 0 or 1. Returns message describing result of attack and
-    and next player who should take a turn.*)
 let handle_turn (r, c) (player : int) =
   let other_player, ship_list_upd, ship_list_og =
     match player with
